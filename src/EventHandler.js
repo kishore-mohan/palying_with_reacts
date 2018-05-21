c7924ab64dc1b5a41da6dda960e0198f2bd05daf
kishore-mohan <kishore.mohan@jifflenow.com>
2018-05-21 17:56:27 +0530

    toogle change

--- src/EventHandler.js
+++ src/EventHandler.js
@@ -3,13 +3,23 @@ import React from 'react'
 class EventHandler extends React.Component {
 	constructor(props){
 		super(props)
-		this.state = {name : 'Kishore'}
+		this.state = {toggle: true, first_name : 'Kishore', last_name : 'Mohan' }
 	}
 
-	clickHandler = (e) =>{
+	clickHandler = (e) => {
 		console.log(e)
-		this.setState({
-			name : 'Mohan'
+		this.setState((prevStage, props) => {
+			console.log(prevStage)
+			return {
+				first_name : 'Mohan',
+				last_name : 'Kishore'
+			} 
+		})
+
+		this.setState((prevStage, props) => {
+			return {
+				toggle : !prevStage.toggle
+			}
 		})
 	}
 
@@ -17,7 +27,8 @@ class EventHandler extends React.Component {
 		return(
 			<div>
 				<button onClick={this.clickHandler}>Click Me! </button>
-				<div>{this.state.name}</div>
+				<div>{this.state.first_name} {this.state.last_name}</div>
+				<div>{this.state.toggle ? 'ON' : 'OFF'}</div>
 			</div>
 			)
 	}
